830b3cf66ceae9e075ff5a2dbedff15ea3316de3
PEDRO HENRIQUE SANTOS BRASILEIRO <pedro.hsbrasileiro@senacsp.edu.br>
2023-05-16 19:46:28 -0300

    correção calculo irpf

--- AdoPoo/src/main/java/com/mycompany/imposto/Irpf.java
+++ AdoPoo/src/main/java/com/mycompany/imposto/Irpf.java
@@ -8,16 +8,16 @@ public class Irpf implements Imposto {
     @Override
     public double calculaImposto(double Salario) {
         
-        if(Salario <= 1903.98){
+        if(Salario <= 2112.00){
             return 0;
-        }else if(Salario >= 1903.99 && Salario <= 2826.65){
-            return ((Salario * 7.5)/100) - 142.80;
+        }else if(Salario >= 2112.01 && Salario <= 2826.65){
+            return ((Salario * 7.5)/100) - 158.40;
         }else if(Salario >= 2826.66 && Salario <= 3751.05){
-            return ((Salario * 15)/100) - 354.80;
+            return ((Salario * 15)/100) - 370.40;
         }else if(Salario >= 3751.06 && Salario <= 4664.68){
-            return ((Salario * 22.5)/100) - 636.13;
+            return ((Salario * 22.50)/100) - 651.73;
         }else{
-            return ((Salario * 27.5)/100) - 869.36;
+            return ((Salario * 27.50)/100) - 884.96;
         }
         
     }
